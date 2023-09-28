c655a13a72dea3e390bacb01ff9241a96f165c31
Wunsyuan Huang <wunsyuan.huang@wunsyuanMacBook-Pro.local>
2023-09-28 22:44:27 +0800

    First Commit

---
+++ src/test/java/com/wunsyuan/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wunsyuan.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
